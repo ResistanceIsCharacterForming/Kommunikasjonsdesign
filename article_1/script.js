033b32248e963120f194803361cf6e4456511fca
fitorevs <142629110+fitorevs@users.noreply.github.com>
2024-09-10 11:56:45 +0200

    .

--- article_1/script.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle")
-const menu = document.getElementById("main-menu")
-
-menuToggle.addEventListener('click', function(event) {
-    event.preventDefault()
-
-    menu.classList.toggle('active')
-})
-
