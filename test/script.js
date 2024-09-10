fe73aa910416441acb7c8838f27185527f239a90
tonjehnord <tonjnord@gmail.com>
2024-09-10 21:46:33 +0200

    ferdig nav

--- test/script.js
+++ test/script.js
@@ -1,9 +1,23 @@
 const menuToggle = document.getElementById("menu-toggle")
-const menu = document.getElementById("main-menu")
 
-menuToggle.addEventListener('click', function(event) {
-    event.preventDefault()
-
-    menu.classList.toggle('active')
+menuToggle.addEventListener('click', () => {
+    const mainMenu = document.getElementById("main-menu")
+    mainMenu.classList.toggle('active')
 })
 
+let lastScrollY = window.scrollY
+
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav')
+    const mainMenu = document.getElementById("main-menu")
+    
+    if (!mainMenu.classList.contains('active')) {
+        if (window.scrollY > lastScrollY) {  
+            nav.classList.add('hidden')
+        } else {
+            nav.classList.remove('hidden')
+        }
+    }
+
+    lastScrollY = window.scrollY
+})
