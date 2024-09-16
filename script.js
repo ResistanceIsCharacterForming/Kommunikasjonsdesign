95367bfdd995cc99cdac3033cb9f35bd7d252bdf
thoraho <thoraho@hiof.no>
2024-09-16 13:19:04 +0200

    startet på xl-artikkel

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const centerLat = 59.128611;
 const centerLong = 11.352778;
 
@@ -8,22 +7,23 @@ const coordinates = [
       center: [centerLat, centerLong],
       zoom: 17.5
    }
-]
+];
 
+// Loop through the coordinates array to set map view and add markers
 coordinates.map((coord) => {
    console.log(coord.center);
    console.log(coord.zoom);
-})
 
+   // Create a map view for each coordinate
+   let map = L.map('map').setView(coord.center, coord.zoom);
 
-let map = L.map('map').setView([coordinates[0].center.map(coord => {
+   // Add tile layer to the map
+   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 20,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+   }).addTo(map);
 
-})], coordinates[0].zoom);
+   // Add a marker for each coordinate
+   L.marker(coord.center).addTo(map);
+});
 
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   maxZoom: 20,
-   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-L.marker([centerLat, centerLong]).addTo(map)
