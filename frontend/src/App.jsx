fa3efb54754150dd14cff7bdc4d83b843f6b2edf
thoraho <thoraho@hiof.no>
2024-09-19 15:10:25 +0200

    BLABLA

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout"
-import "./App.css"
+import Home from "./components/Home"
 
 function App() {
   return (
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>HER SKAL DET VÆRE ARTIKLER OG SÅNT</h1>
+    </div>
+  )
+}
