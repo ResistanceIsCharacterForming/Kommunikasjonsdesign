36c25f61fbad509f5bf07f4cb948c4fce084e69a
thoraho <thoraho@hiof.no>
2024-09-19 15:22:33 +0200

    lagt til array som skal fylles med artikler

---
+++ frontend/src/articles.js
@@ -0,0 +1,9 @@
+const articles = [
+    {
+        title: "Fagbrev - En blindvei for unge i arbeidslivet?",
+        date: "2024-09-07",
+        img: "assets/img/school-7012848_1280.png",
+        alt: "placehold",
+        
+    }
+]
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>HER SKAL DET VÆRE ARTIKLER OG SÅNT</h1>
-    </div>
-  )
+  return <></>
 }
