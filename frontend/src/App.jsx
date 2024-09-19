c0a65a0518d3c04670586ba92f4de5c147fbc440
thoraho <thoraho@hiof.no>
2024-09-19 17:31:14 +0200

    askdkl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home"
 import { useEffect, useState } from "react"
-import article1 from "./article1.json"
+import Card from "./components/Card"
 
 function App() {
   const [article, setArticle] = useState(null)
 
-  article?.map((article) => {
-    console.log(article)
-    article.content.map((content) => {
-      console.log(content)
-    })
-  })
-
-  useEffect(() => {
-    setArticle(article1)
-  }, [])
-
   return (
     <Layout>
+      <Card />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/article" element={<h1>HER KOMMER ARTIKKEL</h1>} />
