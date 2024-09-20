4995f9ea8da1830ef420e8e0ba1f8c710dc07efa
thoraho <thoraho@hiof.no>
2024-09-20 13:36:27 +0200

    JAJAJAJ

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 export default function Card() {
   const articleCards = [
     {
-      id: 1,
-      img: "assets/img/school-7012848_1280.png",
+      id: "article1",
+      img: "/src/assets/img/school-7012848_1280.png",
       date: "2024-09-07",
       title: "Fagbrev - En blindvei for unge i arbeidslivet?",
       text: "Er du ung, har ambisjoner, jobbet hardt – men opplever at ting likevel ikke går som planlagt? Dette er realiteten for mange som tar fagbrev i Norge...",
@@ -13,15 +13,17 @@ export default function Card() {
 
   return (
     <>
-      {articleCards?.map((card, index) => (
-        <article key={crypto.randomUUID()}>
-          <img src={card.img} alt={card.title} />
-          <h3>{card.title}</h3>
-          <p>{card.text}</p>
-          <Link to="/articles/article">Les mer</Link>
-          <time>{card.date}</time>
-        </article>
-      ))}
+      <div id="front-main">
+        {articleCards?.map((card, index) => (
+          <article key={crypto.randomUUID()}>
+            <img src={card.img} alt={card.title} />
+            <time>{card.date}</time>
+            <h3>{card.title}</h3>
+            <p>{card.text}</p>
+            <Link to={`/articles/${card.id}`}>Les mer</Link>
+          </article>
+        ))}
+      </div>
     </>
   )
 }
