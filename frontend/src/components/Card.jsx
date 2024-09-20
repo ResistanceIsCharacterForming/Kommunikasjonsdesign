a4b80ce102ed9b7769a6f5358d545d42ae6428d1
thoraho <thoraho@hiof.no>
2024-09-20 11:05:52 +0200

    asdasd

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom"
 
 export default function Card() {
+  const articleCard = [
+    {
+      img: "assets/img/school-7012848_1280.png",
+      date: "2024-09-07",
+      title: "Fagbrev - En blindvei for unge i arbeidslivet?",
+      text: "Er du ung, har ambisjoner, jobbet hardt – men opplever at ting likevel ikke går som planlagt? Dette er realiteten for mange som tar fagbrev i Norge...",
+    },
+  ]
+
   return (
-    <article>
-      <img src="assets/img/school-7012848_1280.png" alt="School building" />
-      <time dateTime="2024-09-07">7. september 2024</time>
-      <h3>Fagbrev - En blindvei for unge i arbeidslivet? </h3>
-      <p>
-        Er du ung, har ambisjoner, jobbet hardt – men opplever at ting likevel
-        ikke går som planlagt? Dette er realiteten for mange som tar fagbrev i
-        Norge...
-      </p>
-      <Link to="/article">Les mer</Link>
-    </article>
+    <>
+      <article>
+        <div className="card">
+          <img src={articleCard[0].img} alt="School" />
+          <div className="card-content">
+            <p>{articleCard[0].date}</p>
+            <h3>{articleCard[0].title}</h3>
+            <p>{articleCard[0].text}</p>
+            <Link to="/article">Les mer</Link>
+          </div>
+        </div>
+      </article>
+    </>
   )
 }
