1809660761815462b44b4c54a87886a088c6e579
fitorevs <142629110+fitorevs@users.noreply.github.com>
2024-09-24 21:22:31 +0200

    .

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -7,6 +7,9 @@ export default function Layout({ children }) {
         <Nav />
       </header>
       <main>{children}</main>
+      <footer>
+        <p>&copy; 2024 Kommunikasjonsdesign (Gruppe 9)</p>
+      </footer>
     </>
   )
 }
