44e348b0e2a2ca712c81a5604f80f43f22851c20
tonjehnord <tonjnord@gmail.com>
2024-09-25 13:00:15 +0200

    uten /

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css"
 import { BrowserRouter as Router } from "react-router-dom"
 
 createRoot(document.getElementById("root")).render(
-  <Router basename="/Kommunikasjonsdesign/">
+  <Router basename="/Kommunikasjonsdesign">
     <App />
   </Router>
 )
