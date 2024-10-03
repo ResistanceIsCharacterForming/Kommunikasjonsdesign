fc9bc4a73b41a4dda94232b68643eb1fe6d98481
thoraho <thoraho@hiof.no>
2024-10-03 09:04:30 +0200

    pushpush

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout"
 import Home from "./components/Home"
 import Article1 from "./pages/article1"
 import Article2 from "./pages/article2"
+import Article4 from "./pages/article4"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/articles" element={<h3>HER KOMMER ALLE ARTIKLENE</h3>} />
         <Route path="/articles/article1" element={<Article1 />} />
         <Route path="/articles/article2" element={<Article2 />} />
+        <Route path="/articles/article4" element={<Article4 />} />
         <Route path="/about" />
         <Route path="/contact" />
       </Routes>
